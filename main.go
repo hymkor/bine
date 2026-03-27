f19a53ffbab846429c1365b5984371a877bc59a6
HAYAMA Kaoru <3752189+hymkor@users.noreply.github.com>
2026-03-28 02:10:55 +0900

    Remove bold styling from hex view

--- main.go
+++ main.go
@@ -35,15 +35,8 @@ func (app *Application) makeHexOne(pointer *large.Pointer, out *strings.Builder)
 	m := app.editMode
 
 	value := pointer.Value()
-	var on, off string
-	i := pointer.Address() % lineSize
-	if ((i >> 2) & 1) == 0 {
-		on = app.scheme.Cell1[0]
-		off = app.scheme.Cell1[1]
-	} else {
-		on = app.scheme.Cell2[0]
-		off = app.scheme.Cell2[1]
-	}
+	on := app.scheme.Cell[0]
+	off := app.scheme.Cell[1]
 	if pointer.Address() == cursorAddress {
 		if _, ok := app.mark.(marking); ok {
 			m.PrintByte(value, app.scheme.Select[0], app.scheme.Select[1], app.scheme, out)
@@ -63,7 +56,7 @@ func (app *Application) makeHexPart(pointer *large.Pointer, out *strings.Builder
 	var spaceBit uint64 = 1<<0 | 1<<8
 	width := 8 + 3*lineSize + bits.OnesCount64(spaceBit)
 	defer fmt.Fprintf(out, "\x1B[%dG", width+2)
-	fmt.Fprintf(out, "%s%08X%s", app.scheme.Cell2[0], pointer.Address(), app.scheme.Cell2[1])
+	fmt.Fprintf(out, "%s%08X%s", app.scheme.Cell[0], pointer.Address(), app.scheme.Cell[1])
 	for i := 0; i < lineSize; i++ {
 		if (spaceBit & 1) != 0 {
 			out.Write([]byte{' ', ' '})
@@ -139,9 +132,9 @@ func (app *Application) makeAsciiPart(pointer *large.Pointer, out *strings.Build
 			out.WriteRune(c)
 			out.WriteString(app.scheme.Select[1])
 		} else {
-			out.WriteString(app.scheme.Cell1[0])
+			out.WriteString(app.scheme.Cell[0])
 			out.WriteRune(c)
-			out.WriteString(app.scheme.Cell1[1])
+			out.WriteString(app.scheme.Cell[1])
 		}
 		if length == 3 {
 			out.WriteByte(' ')
